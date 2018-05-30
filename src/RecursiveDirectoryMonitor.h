eb1fb46d4d5d3cee7f813278d5f97a4eab6b0fe5
schlenkibus <justus.scheil@parteimail.de>
2018-05-30 12:09:34 +0200

    modified, works now in playground setting

--- src/RecursiveDirectoryMonitor.h
+++ src/RecursiveDirectoryMonitor.h
@@ -6,65 +6,93 @@
 #include <iostream>
 
 namespace FileTools {
-	template<class T>
-class RecursiveDirectoryMonitor : public sigc::trackable {
-		protected:
-			T m_callBack;
-			Glib::RefPtr<Gio::File> m_rootFolder;
-			std::vector<Glib::RefPtr<Gio::FileMonitor>> m_monitors;
-		public:
-			RecursiveDirectoryMonitor(Glib::RefPtr<Gio::File>&& rootFolder, T callback) : m_rootFolder(std::move(rootFolder)),
-																								 m_callBack(callback) {
-				rebuildDirectoryMap();
-			}
+    template<class T>
+    class RecursiveDirectoryMonitor : public sigc::trackable {
+    protected:
+        T m_callBack;
+        Glib::RefPtr<Gio::File> m_rootFolder;
+        std::vector<Glib::RefPtr<Gio::FileMonitor>> m_monitors;
+    public:
+        RecursiveDirectoryMonitor(const Glib::RefPtr<Gio::File>& rootFolder, T callback) : m_rootFolder(rootFolder),
+                                                                                           m_callBack(callback),
+                                                                                           m_monitors(0)
+        {
+          rebuildDirectoryMap();
+        }
 
-			void rebuildDirectoryMap() {
-				m_monitors.clear();
-                addMonitor(m_rootFolder);
-				addSubfolder(m_rootFolder);
-				std::cout << "dirmap is that big: " << m_monitors.size();
-			}
+        void rebuildDirectoryMap()
+        {
+          m_monitors.clear();
+          addMonitor(m_rootFolder);
+          for(auto& dir: getAllDirectorysInDirectory(m_rootFolder))
+          {
+            addMonitor(dir);
+          }
+        }
 
-			void addSubfolder(const Glib::RefPtr<Gio::File>& folder) {
-                try {
-                    auto fileIt = folder->enumerate_children();
-                    while (auto fileInfo = fileIt->next_file()) {
-                        auto file = getFileFromFileInfo(folder, fileInfo);
-                        switch (fileInfo->get_file_type()) {
-                            case Gio::FILE_TYPE_DIRECTORY:
-                                addMonitor(file);
-                                addSubfolder(file);
-                                break;
-                            default:
-                                addMonitor(file);
-                                break;
+        void addMonitor(const Glib::RefPtr<Gio::File>& file)
+        {
+          auto monitor = file->monitor(Gio::FILE_MONITOR_WATCH_MOUNTS);
+          monitor->signal_changed().connect(sigc::mem_fun(this, &RecursiveDirectoryMonitor::onFileChanged));
+          m_monitors.emplace_back(std::move(monitor));
+        }
 
-                        }
-                    }
-                }
-                catch (const Glib::Error &e) {
-                    std::cerr << e.what();
-                } catch(...) {
-                    std::cerr << "unknown exception catched!\n";
-                }
+        void onFileChanged(const Glib::RefPtr<Gio::File>& oldFile,const Glib::RefPtr<Gio::File>& newFile,Gio::FileMonitorEvent monitorEvent)
+        {
+          m_callBack(oldFile, newFile, monitorEvent);
+          rebuildDirectoryMap();
+        }
+
+        std::list<Glib::RefPtr<Gio::File>> getAllFilesBeingMonitored()
+        {
+          return getAllFilesInFolder(m_rootFolder);
+        }
+
+        std::list<Glib::RefPtr<Gio::File>> getAllDirectorysInDirectory(const Glib::RefPtr<Gio::File>& folder)
+        {
+          std::list<Glib::RefPtr<Gio::File>> ret;
+          auto fileIt = folder->enumerate_children();
+          while (auto fileInfo = fileIt->next_file())
+          {
+            auto file = getFileFromFileInfo(folder, fileInfo);
+            if (fileInfo->get_file_type() == Gio::FILE_TYPE_DIRECTORY)
+            {
+              ret.emplace_back(file);
+              for(auto& f: getAllFilesInFolder(file))
+              {
+                ret.emplace_back(f);
+              }
             }
+          }
+          return ret;
+        }
 
-			void addMonitor(const Glib::RefPtr<Gio::File>& file) {
-                auto monitor = file->monitor(Gio::FILE_MONITOR_WATCH_MOUNTS);
-                monitor->signal_changed().connect(sigc::mem_fun(this, &RecursiveDirectoryMonitor::onFileChanged));
-				m_monitors.emplace_back(monitor);
-			}
-
-			void onFileChanged(const Glib::RefPtr<Gio::File>& oldFile,const Glib::RefPtr<Gio::File>& newFile,Gio::FileMonitorEvent monitorEvent) {
-				m_callBack(oldFile, newFile, monitorEvent);
-                rebuildDirectoryMap();
-			}
-
-	protected:
-		Glib::RefPtr<Gio::File> getFileFromFileInfo(const Glib::RefPtr<Gio::File>& currentFolder, const Glib::RefPtr<Gio::FileInfo>& fileInfo) {
-			auto name = fileInfo->get_name();
-			auto path = currentFolder->get_path() + '/' + name;
-			return Gio::File::create_for_path(path);
-		}
-	};
+        std::list<Glib::RefPtr<Gio::File>> getAllFilesInFolder(Glib::RefPtr<Gio::File> folder)
+        {
+          std::list<Glib::RefPtr<Gio::File>> ret;
+          auto fileIt = folder->enumerate_children();
+          while (auto fileInfo = fileIt->next_file()) {
+            auto file = getFileFromFileInfo(folder, fileInfo);
+            if (fileInfo->get_file_type()  == Gio::FILE_TYPE_DIRECTORY)
+            {
+              for(auto f: getAllFilesInFolder(file))
+              {
+                ret.emplace_back(f);
+              }
+            }
+            else
+            {
+              ret.emplace_back(file);
+            }
+          }
+          return ret;
+        }
+    protected:
+        Glib::RefPtr<Gio::File> getFileFromFileInfo(const Glib::RefPtr<Gio::File>& currentFolder, const Glib::RefPtr<Gio::FileInfo>& fileInfo)
+        {
+          auto name = fileInfo->get_name();
+          auto path = currentFolder->get_path() + '/' + name;
+          return Gio::File::create_for_path(path);
+        }
+    };
 }
